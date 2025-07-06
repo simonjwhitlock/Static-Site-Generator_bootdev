8d54021043a5848f8b9b13acea26fabe32889684
simon whitlock <simonjwhitlock@gmail.com>
2025-07-06 15:45:32 +0100

    html node base class created

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from textnode import TextType,TextNode
+from htmlnode import HTMLnode,HTMLTAG
 
 
 def main():
     new_textnode = TextNode("This is some anchor text", TextType.ITALIC, "https://www.boot.dev")
     print("help")
     print(new_textnode.__repr__())
+    new_htmlnode = HTMLnode(tag = HTMLTAG.BOLD, value = "heelp!")
+    print(new_htmlnode.__repr__())
+    
     
 if __name__ == "__main__":
     main()
